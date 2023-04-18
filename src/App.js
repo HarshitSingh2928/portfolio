1ce4850d455ccf4276a77ec25030ea6cf731a83e
Harshit Singh <singhjdk299288@gmail.com>
2023-04-18 13:40:51 +0530

    added headline

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import img from './Images/hs.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars} from '@fortawesome/free-solid-svg-icons'
+import {Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-       < div className="Menu-Top">
+        <Row className='row-menu'> 
+          
+         <div className="Menu-Top">
           <a className="name-logo">
             <img src={img}></img>
           </a>
@@ -16,10 +20,36 @@ function App() {
             <span >Menu</span><FontAwesomeIcon icon={faBars} />
           </a>
        </div>
+       
+       <Col>
+       <div className='headline'>
+        <span>turning ideas into </span>
+        <br></br>
+         <span> real life <span>products</span> </span>
+       <br></br>
+        <span>is my calling.</span>
+       </div>
+       <a className='projects'>View Projects</a>
+       </Col>
+        </Row>
+        <Row>
+        <Col >
+        <div>
+          <h1 className='title'> title</h1>
+        </div>
+        </Col>
+        <Col>
+        <div className='links'>
+         <a>github</a>
+         <a>twitter</a>
+         <a>linkedin</a>
+         <a>leetcode</a>
+        </div>
+        </Col>
+      </Row>
       </header>
-      {/* <div className='headline'>
-        <p>Turning ideas into real life products is my calling</p>
-       </div> */}
+     
+     
     </div>
   );
 }
